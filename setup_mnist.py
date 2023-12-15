610bf2cc5ddc0122be1aad792c71a1e95a85e2d0
daichiofuchi <dai1011q@gmail.com>
2023-12-15 16:04:31 +0900

    model3

--- setup_mnist.py
+++ setup_mnist.py
@@ -94,7 +94,7 @@ class MNISTModel:
         #     model.add(Activation('softmax'))
         # if restore:
         #     model.load_weights(restore)
-        model = tf.keras.models.load_model('/content/drive/My Drive/saved_model/model_2_all.h5')
+        model = tf.keras.models.load_model('/content/drive/My Drive/saved_model/model_3_all.h5')
         layer_outputs = []
         for layer in model.layers:
             if isinstance(layer, Conv2D) or isinstance(layer, Dense):
